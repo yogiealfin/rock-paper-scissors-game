114193ae2d0da22b6261bade123dceb2f299a0f7
yogiealfin <yogiealfin23@gmail.com>
2023-10-25 15:39:55 +0700

    done

---
+++ script.js
@@ -0,0 +1,72 @@
+let score = JSON.parse(localStorage.getItem("score")) || { wins: 0, losses: 0, ties: 0 };
+
+updateScoreElement();
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+  let result = "";
+
+  if (playerMove === "rock") {
+    if (computerMove === "rock") {
+      result = "Tie.";
+    } else if (computerMove === "paper") {
+      result = "You lose.";
+    } else {
+      result = "You win.";
+    }
+  } else if (playerMove === "paper") {
+    if (computerMove === "rock") {
+      result = "You win.";
+    } else if (computerMove === "paper") {
+      result = "Tie.";
+    } else {
+      result = "You lose.";
+    }
+  } else {
+    if (computerMove === "rock") {
+      result = "You lose.";
+    } else if (computerMove === "paper") {
+      result = "You win.";
+    } else {
+      result = "Tie.";
+    }
+  }
+
+  if (result === "You win.") {
+    score.wins += 1;
+  } else if (result === "You lose.") {
+    score.losses += 1;
+  } else {
+    score.ties += 1;
+  }
+
+  // built-in object local storage (only store string value)
+  // save value to local storage
+  localStorage.setItem("score", JSON.stringify(score));
+  updateScoreElement();
+
+  document.querySelector(".js-result").innerHTML = result;
+  document.querySelector(".js-moves").innerHTML = `You
+      <img src="images/${playerMove}-emoji.png" class="move-icon" />
+      <img src="images/${computerMove}-emoji.png" class="move-icon" />
+      Computer`;
+}
+
+function updateScoreElement() {
+  document.querySelector(".js-score").innerHTML = `Wins: ${score.wins}, Loses: ${score.losses}, Ties: ${score.ties}`;
+}
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+  let computerMove = "";
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = "rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = "paper";
+  } else {
+    computerMove = "scissors";
+  }
+
+  return computerMove;
+}
